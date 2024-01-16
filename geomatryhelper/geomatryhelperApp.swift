9cd5c788935b91b8bc41d388a4df71f1ebb5572e
Danika Peet <DPeet@lcs.on.ca>
2024-01-15 20:40:57 -0500

    Initial Commit

---
+++ geomatryhelper/geomatryhelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  geomatryhelperApp.swift
+//  geomatryhelper
+//
+//  Created by Danika Peet on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct geomatryhelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
